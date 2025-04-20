304f3965e533c383f57c3f133c598cd18d3b781c
Swiatoslaw <swiatoslaw.p@gmail.com>
2025-04-20 13:33:30 +0200

    Starting to create the project

---
+++ sources/c++.cc
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------------------------------------------------
+// c++.cc
+//
+// <// <main file, connects main logic>
+
+//
+//---------------------------------------------------------------------------------------------------------------------
+//
+#include "c++.h"
+//---------------------------------------------------------------------------------------------------------------------
+///
+/// This is an example header comment. Here, you explain, what the
+/// function does. Copypaste and adapt it!
+///
+/// @param numbers an array to check
+/// @param length The length of the array. If this text is too long you can
+///
+/// @return the highest number
+//
+int main(void)
+{
+  using namespace std;
+  cout << "Everything works alright.";
+  return 0;
+}
